919e15a27cc1401c6242a4f4124d56af913a634b
Devansh Bhargava <161314637+devanshhhhh14@users.noreply.github.com>
2025-04-11 09:38:03 +0530

    Add files via upload

---
+++ Child.java
@@ -0,0 +1,7 @@
+// Child class that overrides show() method of Mother class
+class Child extends Mother {
+    @Override
+    void show() {
+        System.out.println("Child's show() method");
+    }
+}
---
+++ mother.java
@@ -0,0 +1,6 @@
+// Mother class with non-static show() method
+class Mother {
+    void show() {
+        System.out.println("Mother's show() method");
+    }
+}
